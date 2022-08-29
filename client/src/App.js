ef19e365bf85a2a5d92e4b9bbbd35a6f0e8f9106
Teagrin Forde <teagrinb@gmail.com>
2022-08-29 18:44:38 -0400

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,9 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
-import Home from "./pages/Home.js"
+import Home from "./pages/Home.js";
+import Login from './pages/Login';
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -41,6 +43,10 @@ function App() {
               <Route 
                 path="/" 
                 element={<Home />}
+              />
+              <Route 
+                path="/login"
+                element={<Login />}
               />
             </Routes>
           {/* </div>
