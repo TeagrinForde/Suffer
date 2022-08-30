4776736bcdde7b17fff09dda39638e782339534e
Alkarias <alkarias29@gmail.com>
2022-08-30 10:28:14 -0400

    link to login page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,14 @@
 import React from 'react';
 // import Typical from "react-typical";
-import { Navigate } from 'react-router-dom';
 // import { useQuery } from '@apollo/client';
 
 // import { QUERY_PROFILES } from '../utils/queries';
 const title = '<SUFFER/>';
 const Home = () => {
 
-  function nextScreen() {
-    console.log('click');
-    // function to switch to the next screen
-    return <Navigate to="/profile"/>
-  }
 
   return (
-  <div onClick={nextScreen} className="p-auto" id='homeContainer'>
+  <div onClick={document.location.pathname = "/login"} className="p-auto" id='homeContainer'>
     <div>
       <h1 className="" id='homeTitle'>
         {title}
