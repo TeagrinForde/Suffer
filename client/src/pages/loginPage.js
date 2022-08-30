6acee424b0228ae4597babd364df5f8c65631c54
Teagrin Forde <teagrinb@gmail.com>
2022-08-30 14:11:58 -0400

    login and signup style

--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -20,14 +20,14 @@ function LoginPage() {
         const signupBtn = document.getElementById('signUp');
         loginBtn.classList.remove('focus');
         signupBtn.classList.remove('focus');
-        // // add appropriate focus
+        // add appropriate focus
         const fcsBtn = document.getElementById(tab);
         fcsBtn.classList.add('focus');
     }, [tab]);
 
     return (
         <div className="container" class="align-middle" id='loginContainer'>
-            <div class="col-auto">
+            <div >
                 <button id="login" onClick={() => setTab('login')}>Login</button>
                 <button id="signUp" onClick={() => setTab('signUp')}>Sign Up</button>
             </div>
