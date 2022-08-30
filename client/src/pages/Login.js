aa9d95959c0560c0d1957a9dc7321f6d8a8d3705
Teagrin Forde <teagrinb@gmail.com>
2022-08-30 10:28:18 -0400

    merge build

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useMutation } from "@apollo/client";
-import { LOGIN_USER, ADD_USER } from "../utils/mutations";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from "../utils/auth";
+import Auth from '../utils/auth';
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ username: "", password: "" });
+  const [formState, setFormState] = useState({ username: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -35,88 +35,55 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      username: "",
-      password: "",
+      username: '',
+      password: '',
     });
   };
 
   return (
-    <main className="container mb-4">
+    <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
-        <div className="card bg-dark">
-          <ul class="nav-tabs d-flex" id="loginTabs" role="tablist">
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                id="login"
-                data-toggle="tab"
-                href="#login-tab"
-                role="tab"
-                aria-controls="login"
-                aria-selected="true"
-              >
-                <h4 className="bg-dark text-light p-2">Login</h4>
-              </a>
-            </li>
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                You are logged in! Time to{' '}
+                <Link to="/">SUFFER.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your username"
+                  name="username"
+                  type="text"
+                  value={formState.username}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Login
+                </button>
+              </form>
+            )}
 
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                id="signup"
-                data-toggle="tab"
-                href="#signup-tab"
-                role="tab"
-                aria-controls="signup"
-                aria-selected="false"
-              >
-                <h4 className="bg-dark text-light p-2">Sign Up</h4>
-              </a>
-            </li>
-          </ul>
-
-          <div class="tab-content" id="nav-tabContent">
-            <div className="card-body" class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="login">
-              {data ? (
-                <p>
-                  You are logged in! You may now head{" "}
-                  <Link to="/">back to the homepage.</Link>
-                </p>
-              ) : (
-                <form onSubmit={handleFormSubmit}>
-                  <input
-                    className="form-input p-2"
-                    placeholder="Your username"
-                    name="username"
-                    type="text"
-                    value={formState.username}
-                    onChange={handleChange}
-                  />
-                  <input
-                    className="form-input p-2"
-                    placeholder="******"
-                    name="password"
-                    type="password"
-                    value={formState.password}
-                    onChange={handleChange}
-                  />
-                  <button
-                    className="btn btn-block btn-info p-2"
-                    style={{ cursor: "pointer" }}
-                    type="submit"
-                  >
-                    Login
-                  </button>
-                </form>
-              )}
-
-              {error && (
-                <div className="my-3 p-3 bg-danger text-white">
-                  {error.message}
-                </div>
-              )}
-            </div>
-
-
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
           </div>
         </div>
       </div>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
@@ -9,10 +8,9 @@ import Auth from '../utils/auth';
 const Signup = () => {
   const [formState, setFormState] = useState({
     name: '',
-    email: '',
     password: '',
   });
-  const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -30,11 +28,11 @@ const Signup = () => {
     console.log(formState);
 
     try {
-      const { data } = await addProfile({
+      const { data } = await addUser({
         variables: { ...formState },
       });
 
-      Auth.login(data.addProfile.token);
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
@@ -74,7 +72,7 @@ const Signup = () => {
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
-                  Submit
+                  Sign Up
                 </button>
               </form>
             )}
