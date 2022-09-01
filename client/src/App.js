d10810cd1f0d3270b8900e1261c07a1f64a46666
Hunter Padgett <gizmokabanjo@gmail.com>
2022-09-01 10:38:31 -0400

    file stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,72 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Breakout from "./games/breakout";
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+
+import Home from "./pages/Home.js";
+import LoginPage from './pages/LoginPage.js';
+import Profile from "./pages/Profile.js";
+import Header from "./components/Header.js";
+import Game from "./pages/Game.js";
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Breakout />
-        </Route>
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        {/* <div className="flex-column justify-flex-start h-100"> */}
+          
+          <div className="entire">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />}
+              />
+              <Route 
+                path="/login"
+                element={<LoginPage />}
+                />
+              <Route
+              path="/profile"
+              element={[<Header />, <Profile/>]}
+              />
+              <Route
+              path="/game"
+              element={<Game/>}
+              />
+            </Routes>
+          </div>
+        {/*</div> */}
+
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ willClient/src/components/ScoreBoard.js
@@ -0,0 +1,9 @@
+const ScoreBoard = ({ score }) => {
+  return (
+    <div className="candyScore">
+      <h2>{score}</h2>
+    </div>
+  )
+}
+
+export default ScoreBoard
